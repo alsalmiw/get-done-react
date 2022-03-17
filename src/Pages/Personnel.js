5c5843cb7875c7c0ba2b9446fcfccbb26d684276
Jesse <JesseAbarca888@gmail.com>
2022-03-16 21:17:32 -0700

    Personnel Tables with filtering and mapping done

--- src/Pages/Personnel.js
+++ src/Pages/Personnel.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Row, Col, Table, Form, Accordion } from 'react-bootstrap';
 
 function Personnel() {
-
+  
   //map through the users to create the table 
+  
+  //The data has to be an array for the map method to
+  let DummyData = [
+    {Id: 1, Username: "Jesse", role: "Project Manager"},
+    {Id: 2, Username: "Walaa", role: "admin"},
+    {Id: 3, Username: "Peter", role: "Project Manager"},
+    {Id: 4, Username: "Jovann", role: "Specialist"},
+    {Id: 5, Username: "Danny", role: "Specialist"},
+    {Id: 6, Username: "An", role: "Specialist"},
+    {Id: 7, Username: "John", role: "Revoked Access"},
+    {Id: 8, Username: "Jamie", role: "Revoked Access"},
 
+  ];
 
-  let DummyData = {
-    User1: {
-      Id: 1,
-      Username: "Jesse",
-      Role: "Admin",
-    },
-    User2: {
-      Id: 2,
-      Username: "Peter",
-      Role: "Admin",
-    },
-    User3: {
-      Id: 3,
-      Username: "Peter",
-      Role: "Admin",
-    },
-  };
+  let DummyDataRevokedAccess = [
+    {Id: 1, Username: "Jesse", role: "Revoked Access"},
+    {Id: 7, Username: "John", role: "Revoked Access"},
+  ];
+
+  useEffect(async () => {
+    SetUserPersonnel(DummyData);
+  }, [])
 
   const [Personnel, SetUserPersonnel] = useState([]);
 
-
   return (
-
+    
     <Container>
       <Row>
         <Col className="mt-4">
@@ -46,19 +48,27 @@ function Personnel() {
                     </tr>
                   </thead>
                   <tbody>
-                      <tr>
-                      <td>1</td>
-                      <td>Mark</td>
-                      <td>Admin</td>
-                      <td>
-                        <Form.Select aria-label="Default select example">
-                          <option>Open this select menu</option>
-                          <option value="1">One</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                        </Form.Select>
-                      </td>
-                      </tr> 
+                    {
+                      Personnel.map((user, i) => {
+                        return (
+                          user.role != "Revoked Access" ?
+                          <tr>
+                          <td>{user.Id}</td>
+                          <td>{user.Username}</td>
+                          <td>{user.role}</td>
+                          <td>
+                            <Form.Select aria-label="Default select example">
+                              <option>Open this select menu</option>
+                              <option value="1">One</option>
+                              <option value="2">Two</option>
+                              <option value="3">Three</option>
+                            </Form.Select>
+                          </td>
+                          </tr> 
+                          : null
+                        )
+                      })
+                    }
                   </tbody>
                 </Table>
               </Accordion.Body>
@@ -66,13 +76,38 @@ function Personnel() {
             <Accordion.Item eventKey="1">
               <Accordion.Header>Users With Revoked Access</Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                est laborum.
+              <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Username</th>
+                      <th>Role</th>
+                      <th>Change Role</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      Personnel.map((user, i) => {
+                        return (
+                          user.role === "Revoked Access" ?
+                          <tr>
+                          <td>{user.Id}</td>
+                          <td>{user.Username}</td>
+                          <td>{user.role}</td>
+                          <td>
+                            <Form.Select aria-label="Default select example">
+                              <option>Open this select menu</option>
+                              <option value="1">Project Manager</option>
+                              <option value="2">Specialist</option>
+                            </Form.Select>
+                          </td>
+                          </tr> 
+                          :null
+                        )
+                      })
+                    }
+                  </tbody>
+                </Table>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
